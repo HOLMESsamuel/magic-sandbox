56aced264ffe0a44f885aa4d2a5ac832ed4fc421
Samuel Holmes <samuel.holmes-ext@pole-emploi.fr>
2023-12-27 16:10:16 +0100

    style connection page

--- magic-sandbox-front/src/ConnectionPage.vue
+++ magic-sandbox-front/src/ConnectionPage.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <h1>Connection</h1>
+  <div class="connection-div">
+    <h1>Magic sandbox</h1>
     <input v-model="name" placeholder="Enter your name" />
     <input v-model="roomId" placeholder="Enter Room ID" />
     <button @click="joinRoom">Join</button>
@@ -25,5 +25,51 @@ export default {
 </script>
 
 <style>
-/* Add styles for your connection page here */
+body, html {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
+
+.connection-div {
+  background-color: #48494b; /* Dark background */
+  font-family: 'Trebuchet MS', sans-serif;
+  color: #f4f4f4;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+.connection-div input {
+  background-color: #1c1e22;
+  border: 2px solid #3a3b3e;
+  border-radius: 4px;
+  color: #fff;
+  margin: 10px;
+  padding: 12px 20px;
+  width: 250px;
+}
+
+.connection-div button {
+  background-color: #4c5058;
+  border: none;
+  border-radius: 4px;
+  color: white;
+  cursor: pointer;
+  font-size: 16px;
+  margin: 10px;
+  padding: 12px 20px;
+  transition: background-color 0.3s, transform 0.3s;
+}
+
+.connection-div button:hover {
+  background-color: #62666d;
+}
+
+.connection-div button:active {
+  transform: scale(0.98);
+}
+
 </style>

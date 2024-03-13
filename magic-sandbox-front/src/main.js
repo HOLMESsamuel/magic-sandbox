c5b372b33fae5fcbf12679a3bbc890774ecde0d9
Samuel Holmes <samuel.holmes-ext@pole-emploi.fr>
2024-03-13 07:58:59 +0100

    fix the sticky card bug

--- magic-sandbox-front/src/main.js
+++ magic-sandbox-front/src/main.js
@@ -1,6 +1,30 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import { createStore } from 'vuex'
 
-createApp(App).use(router).mount('#app');
+// Create a new store instance.
+const store = createStore({
+  state () {
+    return {
+        currentlyDraggingCardId : null,
+        startDragPosition : null,
+        cardOffset : {x : 0, y : 0}
+    }
+  },
+  mutations: {
+    startDragging (state, {cardId, startDragPosition, cardOffset}) {
+      state.currentlyDraggingCardId = cardId;
+      state.startDragPosition = startDragPosition;
+      state.cardOffset = cardOffset;
+    },
+    endDragging (state) {
+        state.currentlyDraggingCardId = null;
+        state.startDragPosition = null;
+        state.cardOffset = {x : 0, y : 0};
+    }
+  }
+})
+
+createApp(App).use(router).use(store).mount('#app');
 

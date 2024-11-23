49fcf991b40de98025556fe2c3bc9124f32fda46
Samuel Holmes <samuel.holmes-ext@pole-emploi.fr>
2024-11-23 13:46:02 +0100

    add caching strategy for images

--- magic-sandbox-front/vite.config.js
+++ magic-sandbox-front/vite.config.js
@@ -1,6 +1,28 @@
-import vue from '@vitejs/plugin-vue'
+// vite.config.js
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
-export default ({
-  plugins: [vue()],
-})
+export default defineConfig({
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /\.(?:png|jpg|jpeg|svg|gif)$/,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'image-cache',
+              expiration: {
+                maxEntries: 400,
+                maxAgeSeconds: 60 * 60 * 24 * 30,
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
+});
